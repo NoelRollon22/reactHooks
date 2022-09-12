79e8c12a17568fea9717f18279c3053f251d0adc
Noel Rollon <noel.rollon@oocl.com>
2022-09-12 11:31:21 +0800

    change text using onClick button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react';
+
 
 function App() {
+  const [favorite, setFavorite] = useState ("singing");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>My Favorite thing to do is {favorite}!</h1>
+      <button onClick={()=>setFavorite("reading")}>Reading</button>
+      <button onClick={()=>setFavorite("singing")}>Singing</button>
+      <button onClick={()=>setFavorite("dancing")}>Dancing</button>
+      <button onClick={()=>setFavorite("watching movies")}>Watching Movies</button>
+      
     </div>
   );
 }
-
 export default App;
+
